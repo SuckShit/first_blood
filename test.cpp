d2c81974ce0fa3db7ca07d6264d0f4e089c214a7
SuckShit <satansavior@gmail.com>
2018-08-22 18:13:50 +0800

    traits test

--- test.cpp
+++ test.cpp
@@ -590,6 +590,56 @@ T returntestfun(T t)
 	return t;
 }
 
+template<typename T>
+struct function_traits;
+
+//普通函数
+template<typename Ret, typename... Args>
+struct function_traits<Ret(Args...)>
+{
+public:
+	enum { arity = sizeof...(Args) };
+	typedef Ret function_type(Args...);
+	typedef Ret return_type;
+	using stl_function_type = std::function<function_type>;
+	typedef Ret(*pointer)(Args...);
+
+	template<size_t I>
+	struct args
+	{
+		static_assert(I < arity, "index is out of range, index must less than sizeof Args");
+		using type = typename std::tuple_element<I, std::tuple<Args...>>::type;
+	};
+};
+
+//函数指针
+template<typename Ret, typename... Args>
+struct function_traits<Ret(*)(Args...)> : function_traits<Ret(Args...)> {};
+
+//std::function
+template <typename Ret, typename... Args>
+struct function_traits<std::function<Ret(Args...)>> : function_traits<Ret(Args...)> {};
+
+//member function
+#define FUNCTION_TRAITS(...) \
+    template <typename ReturnType, typename ClassType, typename... Args>\
+    struct function_traits<ReturnType(ClassType::*)(Args...) __VA_ARGS__> : function_traits<ReturnType(Args...)>{}; \
+
+FUNCTION_TRAITS()
+FUNCTION_TRAITS(const)
+FUNCTION_TRAITS(volatile)
+FUNCTION_TRAITS(const volatile)
+
+//函数对象
+template<typename Callable>
+struct function_traits : function_traits<decltype(&Callable::operator())> {};
+
+template<typename T>
+void PrintType()
+{
+	cout << typeid(T).name() << endl;
+}
+
 int main()
 {
 	doOperation<add1>();
@@ -627,5 +677,11 @@ int main()
 	//time_std_function<func::function<void(float)>>(argc, "func::function");
 	func::function<void(int)> testmyfunc;
 	testmyfunc = add1;
+
+	std::function<int(int)> fff = [](int a) {return a; };
+	PrintType<function_traits<std::function<int(int)>>::function_type>(); //将输出int __cdecl(int)
+	PrintType<function_traits<std::function<int(int)>>::args<0>::type>();//将输出int
+	PrintType<function_traits<decltype(fff)>::function_type>();//将输出int __cdecl(int)
+
 	return 0;
 }
