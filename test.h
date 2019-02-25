361b1c08eb0dd94597a5f282fafc13f746865d37
SuckShit <satansavior@gmail.com>
2019-02-25 17:52:38 +0800

    LC72 SUBMIT

--- test.h
+++ test.h
@@ -2458,3 +2458,42 @@ public:
 //	}
 //	return 0;
 //}
+
+//dp[i][j] record minimum steps convert word1[0~i] to word2[0~j]
+//and dp[i][0] = i dp[0][j] = j
+//so 1.word1[i]=word2[j]  dp[i][j] = dp[i-1][j-1]
+//   2.word1[i]!=word2[j] 
+//			1.dp[i][j] = dp[i-1][j-1] + 1 replace
+//			2.word1[i-1]=word2[j] delete word[i] dp[i][j] = dp[i-1][j] + 1
+//			3.word1[i]=word2[j-1] add word[j]  dp[i][j] = dp[i][j-1] + 1
+class SolutionLC72 {
+public:
+	int minDistance(string word1, string word2) {
+		int m = word1.length(), n = word2.length();
+		vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+		for (int i = 0; i <= m; i++)
+		{
+			dp[i][0] = i;
+		}
+		for (int j = 0; j <= n; j++)
+		{
+			dp[0][j] = j;
+		}
+		for (int i = 1; i <= m; i++)
+		{
+			for (int j = 1; j <= n; j++)
+			{
+				if (word1[i - 1] == word2[j - 1])
+				{
+					dp[i][j] = dp[i - 1][j - 1];
+				}
+				else
+				{
+					dp[i][j] = dp[i - 1][j - 1] < dp[i - 1][j] ? (dp[i - 1][j - 1] < dp[i][j - 1] ? dp[i - 1][j - 1] + 1 :
+						dp[i][j - 1] + 1) : (dp[i - 1][j] < dp[i][j - 1] ? dp[i - 1][j] + 1 : dp[i][j - 1] + 1);
+				}
+			}
+		}
+		return dp[m][n];
+	}
+};
